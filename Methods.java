748138b0b48676442a4fe9a4e4cdada1f8df7336
fators22 <116498723+fators22@users.noreply.github.com>
2022-10-23 11:08:51 -0400

    Add files via upload
    Project description is added.

---
+++ Methods.java
@@ -0,0 +1,331 @@
+/**
+ *  Do not use any of these methods to solve other methods.
+ *  Do not create any other methods to solve these methods.
+ *  Do not change the method headers (return type, name, parameters) of these methods.
+ *  Do not delete the comments from the starter code
+ *  Do not use if-statements or loops to solve these problems.
+ *
+ *  Remember: Comments describe the method that comes after them.
+ */
+public class Methods
+{
+  /** The starting point of this program.
+   *
+   *  Write code here to test the methods you write.
+   *
+   *  @param String[] args Commandline arguments.
+   */
+  public static void main(String[] args)
+  {
+    // You need to test your work! Follow this example of how to test a method:
+
+    // This statement invokes the hello method and stores its response in the answer variable
+    String answer = hello("World");
+
+    // This statement outputs the value of the answer variable to the console
+    System.out.println( answer );
+
+    // When you run this code it will call the hello method and output the results.
+    // Since the hello method is being passed the value "World" you should expect
+    // the answer to be "Hello World" (see the comment of the hello method below)
+    // When you run this program you should look at the output and verify that it is correct!
+    // If it is not correct, then the method is not correct and you should keep working on it.
+
+    // You should change this code, or add additional code to this method to test each
+    // method as you write it. Follow this pattern:
+    //  1. Call the method you want to test and store its return value in a variable
+    //  2. Use System.out.println to output that variable
+
+    // Note: Make sure you declare the variable correctly. Its type should match the return type of the method.
+
+    // If you are getting compile errors for your test code in this method then check these things:
+    //  - Did you write the method name correctly?
+    //    Try copying and pasting the method name from the method header.
+    //  - Are the arguments you are passing to the method correct?
+    //    Look at how many parameters the method has and the types of those variables.
+    //  - Is the type of your answer variable correct?
+    //    Look at the return type of the method, this should match the type of your answer variable.
+  }
+
+  /**  Use the distance formula to calculate the distance between (x1, y1) and (x2, y2)
+   *
+   *  Use the distance formula: https://www.google.com/search?q=distance+formula
+   *
+   *   Example:
+   *     x1      -> 1
+   *     y1      -> 2
+   *     x2      -> 3
+   *     y2      -> 4
+   *     return  -> 2.82842712475
+   *
+   *   @param int x1
+   *   @param int y1
+   *   @param int x2
+   *   @param int y2
+   *   @return double The distance between (x1, y1) and (x2, y2)
+   */
+  public static double calculateDistance(int x1, int y1, int x2, int y2)
+  {
+    return -1.0;
+  }
+
+  /**  Calculate the distance between 16 and x. This will always be a positive number.
+   *
+   *
+   *   Example:
+   *     x       -> 5
+   *     return  -> 11
+   *
+   *     x       -> 16
+   *     return  -> 0
+   *
+   *     x       -> 20
+   *     return  -> 4
+   *
+   *   @param int x The value to use when calcualting the distance from 16
+   *   @return int The distance between x and 16
+   */
+  public static int distanceFrom16(int x)
+  {
+    return -999;
+  }
+
+  /**  Say "Hello [name]"
+   *
+   *   Example:
+   *     name    -> "Tacocat"
+   *     return -> "Hello Tacocat"
+   *
+   *   @param String name The name to say hello to
+   *   @return String "Hello [name]"
+   */
+  public static String hello(String name)
+  {
+    return null;
+  }
+
+  /**  Return a new string that repeats str 3 times
+   *
+   *   Example:
+   *     str    -> "tacos"
+   *     return -> "tacostacostacos"
+   *
+   *   @param String str The string to repeat 3 times
+   *   @return String str repeated three times
+   */
+  public static String triple(String str)
+  {
+    return null;
+  }
+
+  /**  Find the index of needle in haystack or return -1 if needle is not in haystack
+   *
+   *   Example:
+   *     needle     -> "aco"
+   *     haystack   -> "tacos"
+   *     return     -> 1
+   *
+   *   @param String needle The string to find in haystack
+   *   @param String haystack The string to find where needle is located
+   *   @return String The index of needle in haystack, or -1 if needle is not in haystack
+   */
+  public static int findNeedle(String needle, String haystack)
+  {
+    return -999;
+  }
+
+  /**  Return the single character that is located at the specified index
+   *
+   *   Example:
+   *     str     -> "tacos"
+   *     index   -> 2
+   *     return  -> "c"
+   *
+   *   @param String str The string to isolate a character from
+   *   @param int index The index of the character to isolate
+   *   @return String The single character located at specific index
+   *
+   *   Precondition: index >= 0 and index <= str.length()
+   */
+  public static String isolateCharacterAtIndex(String str, int index)
+  {
+    return null;
+  }
+
+  /**  Remove the character at index from str
+   *
+   *   Example:
+   *     needle    -> "tacos"
+   *     index     -> 2
+   *     return    -> "taos"
+   *
+   *   @param String str The string to remove a character from
+   *   @param int index The index of the character to remove
+   *   @return String str with the character at index removed
+   *
+   *   Precondition: index >= 0 and index <= str.length()
+   */
+  public static String removeCharacter(String str, int index)
+  {
+    return null;
+  }
+
+  /**  Insert needle into haystack at the specified index
+   *
+   *   Example:
+   *     needle    -> "CHEESE"
+   *     haystack  -> "tacos"
+   *     index     -> 2
+   *     return    -> "taCHEESEcos"
+   *
+   *   @param String needle The value to insert into haystack
+   *   @param String haystack The string to insert needle into
+   *   @param int index The index where needle should be inserted into haystack
+   *   @return String The result of inserting needle into haystack
+   *
+   *   Precondition: index >= 0 and index <= haystack.length()
+   */
+  public static String insertNeedle(String needle, String haystack, int index)
+  {
+    return null;
+  }
+
+  /**  Return the index that comes after the end of needle in haystack
+   *
+   *   Example:
+   *     needle     -> "aco"
+   *     haystack   -> "tacos"
+   *     return     -> 4
+   *
+   *   @param String needle The string to locate in haystack
+   *   @param String haystack The string that contains needle
+   *   @return int The index that comes after the needle
+   *
+   *   Precondition: needle is contained within haystack
+   */
+  public static int findEndOfNeedle(String needle, String haystack)
+  {
+    return -999;
+  }
+
+   /**  Remove the needle from the haystack
+    *
+    *   Example:
+    *     needle    -> "co"
+    *     haystack  -> "tacos"
+    *     return    -> "tas"
+    *
+    *   @param String needle The value to remove from haystack
+    *   @param String haystack The string to remove the needle from
+    *   @return String The result of removing needle from haystack
+    */
+   public static String removeNeedle(String needle, String haystack)
+   {
+     return null;
+   }
+
+   /**  Given a string in the format "I like X", identify the value of X
+    *
+    *   Example:
+    *     needle    -> "I like tacos"
+    *     return    -> "tacos"
+    *
+    *     needle    -> "I like writing code"
+    *     return    -> "writing code"
+    *
+    *   @param String str A string that says "I like X"
+    *   @return String The value X from the string "I like X"
+    *
+    *   Precondition: str starts with "I like "
+    */
+   public static String whatILike(String str)
+   {
+     return null;
+   }
+
+   /**  Return the first N characters from str
+    *
+    *   Example:
+    *     needle    -> "tacos"
+    *     n         -> 2
+    *     return    -> "ta"
+    *
+    *   @param String str The string to get the characters from
+    *   @param int n The number of characters to return from str
+    *   @return String The first N characters of str
+    *
+    *   Precondition: n >=0 and n <= str.length()
+    */
+   public static String getFirstNCharacters(String str, int n)
+   {
+     return null;
+   }
+
+   /**  Remove the first N characters from str
+    *
+    *   Example:
+    *     needle    -> "tacos"
+    *     n         -> 2
+    *     return    -> "cos"
+    *
+    *   @param String str The string to remove characters from the beginning of
+    *   @param int n The number of characters to remove from str
+    *   @return String The string str with the first n characters removed
+    *
+    *   Precondition: n >=0 and n <= str.length()
+    */
+   public static String removeFirstNCharacters(String str, int n)
+   {
+     return null;
+   }
+
+   /**  Return the last N characters from str
+    *
+    *   Example:
+    *     needle    -> "tacos"
+    *     n         -> 2
+    *     return    -> "os"
+    *
+    *   @param String str The string to get the characters from
+    *   @param int n The number of characters to return from str
+    *   @return String The last N characters of str
+    *
+    *   Precondition: n >=0 and n <= str.length()
+    */
+   public static String getLastNCharacters(String str, int n)
+   {
+     return null;
+   }
+
+   /**  Remove the last N characters from str
+    *
+    *   Example:
+    *     needle    -> "tacos"
+    *     n         -> 2
+    *     return    -> "tac"
+    *
+    *   @param String str The string to remove characters from the end of
+    *   @param int n The number of characters to remove from str
+    *   @return String The string str with the last n characters removed
+    *
+    *   Precondition: n >=0 and n <= str.length()
+    */
+   public static String removeLastNCharacters(String str, int n)
+   {
+     return null;
+   }
+
+   /**  Swap the fisrt and last characters of a string
+    *
+    *   Example:
+    *     str    -> "tacos"
+    *     return -> "sacot"
+    *
+    *   @param String str The string to swap the first and last characters of
+    *   @return String str with the first and last characters swapped
+    */
+   public static String frontBack(String str)
+   {
+     return null;
+   }
+}
--- README.md
+++ README.md
@@ -1,1 +1,208 @@
-# javaMethods
+# Lab 1.2 MethodDrills
+
+## Instructions
+
+Complete each of the methods in MethodDrills.java so that they do what their comments say they should do.
+
+Write code in the main method to test your methods as you complete them. It is recommended that you write one method, test it, fix it, then move on to another method.
+
+You will be using Math and String methods to solve these problems.
+
+### Math Methods
+
+```
+Math.sqrt(double a)
+Math.pow(double base, double exponent)
+Math.abs(int a)
+Math.abs(double a)
+```
+
+### String Methods
+
+```
+int length()
+int indexOf(String needle)
+String substring(int start, int end)
+String substring(int start)
+```
+
+Your code must meet the following restrictions:
+
+*  Do not use any of these methods to solve other methods.
+*  Do not create any other methods to solve these methods.
+*  Do not change the method headers (return type, name, parameters) of these methods.
+*  Do not delete the comments from the starter code
+*  Do not use if-statements or loops to solve these problems.
+
+Remember that comments describe the method that comes after them.
+
+If you get stuck, there are hints for each method at the end of this document.
+
+## Testing
+
+You need to test your work! Follow this example of how to test a method:
+
+This statement invokes the hello method and stores its response in the answer variable
+
+```
+String answer = hello("World"); // invoke the hello method and stores its response in the answer variable
+System.out.println( answer );   // output the value of the answer variable to the console
+```
+
+When you run this code it will call the hello method and output the results. Since the hello method is being passed the value "World" you should expect the answer to be "Hello World" (see the comment of the hello method). When you run this program you should look at the output and verify that it is correct! If it is not correct, then the method is not correct and you should keep working on it.
+
+You should write code in the main method to test each method as you write it. Follow this pattern:
+
+* Call the method you want to test and store its return value in a variable
+* Use System.out.println to output that variable
+
+Note: Make sure you declare the variable correctly. Its type should match the return type of the method.
+
+If you are getting compile errors for your test code in this method then check these things:
+
+* Did you write the method name correctly? Try copying and pasting the method name from the method header.
+* Are the arguments you are passing to the method correct? Look at how many parameters the method has and the types of those variables.
+* Is the type of your answer variable correct? Look at the return type of the method, this should match the type of your answer variable.
+
+## Grading
+
+You must *correctly complete at least 8* of these methods to pass this lab. If you correctly complete *all* of these methods, then you will earn 1 Above & Beyond point.
+
+These methods will be tested by checking their output against several test cases. If you method fails any of those test cases it will be deemed *incorrect*.
+
+For example: Consider the following method:
+
+```
+int subtract(int a, int b)
+```
+
+This method should return the difference between a and b so the following method call should result in the specified value:
+
+```
+subtract(3, 5) -> -2
+```
+
+If your version of this method returned the value 2, then it would be marked as *incorrect*.
+
+## Methods
+
+The instructions for each of these methods are located in the comments that precede them in the java source code file. The comments also include at least one example of what the method should return given some inputs.
+
+It is recommended that you complete these methods in the order they are presented, however none of these methods have a prerequisite so you can complete them in any order you like.
+
+```
+double calculateDistance(int x1, int y1, int x2, int y2)
+int distanceFrom16(int x)
+String hello(String name)
+String triple(String str)
+int findNeedle(String needle, String haystack)
+String isolateCharacterAtIndex(String str, int index)
+String removeCharacter(String str, int index)
+String insertNeedle(String needle, String haystack, int index)
+int findEndOfNeedle(String needle, String haystack)
+String removeNeedle(String needle, String haystack)
+String whatILike(String str)
+String getFirstNCharacters(String str, int n)
+String removeFirstNCharacters(String str, int n)
+String getLastNCharacters(String str, int n)
+String removeLastNCharacters(String str, int n)
+String frontBack(String str)
+```
+
+## Turning it in
+
+When you have finished this lab, upload it to [https://turnerinner.com/@mrmaycs](https://turnerinner.com/@mrmaycs)
+
+## Hints
+
+### calculateDistance(int x1, int y1, int x2, int y2)
+
+You need to use the Math methods to find square root and powers.
+
+### distanceFrom16(int x)
+
+You need to make sure the answer you get is always positive. That's exactly what the Absolute Value method does!
+
+### hello(String name)
+
+This is a simple concatenation problem.
+
+### triple(String str)
+
+This is also a simple concatenation problem!
+
+### findNeedle(String needle, String haystack)
+
+What string method tells you *where* a needle is?
+
+### isolateCharacterAtIndex(String str, int index)
+
+What string method can get a small string from a big string? You are given the index of where the character is located, how can you use math to calculate the index where that character ends?
+
+### removeCharacter(String str, int index)
+
+This problem is the inverse of the isolateCharacterAtIndex problem.
+
+You should think of the string as a combination of three pieces. A beginning, a character in the middle, and an ending.
+
+Consider the example removeCharacter("TACOS", 2) You can think of the string TACOS as being separated into three parts:
+
+```
+TA C OS
+```
+
+You need to isolate the beginning part (what index does it start at? What index does it end at?) and you need to isolate the ending part (what index does it start at? What index does it end at?). Then you can concatenate those two pieces together.
+
+### insertNeedle(String needle, String haystack, int index)
+
+This problem is similar to the removeCharacter problem, except you are breaking the string into two pieces and then inserting the third piece inbetween them.
+
+Consider the example insertNeedle("CHEESE", "tacos", 2). You should break the string into two pieces.
+
+```
+tac os
+```
+
+Where does the first piece start? Where does it end? Where does the second piece start? Where does it end? Once you have solved that problem, this becomes a simple concatenation problem.
+
+### findEndOfNeedle(String needle, String haystack)
+
+You know how to find the index where needle starts in haystack (remember how you solved the findNeedle method) Now you need to do some math to figure out where that needle ends. What string method can you use to identify how many characters are in the needle string?
+
+### removeNeedle(String needle, String haystack)
+
+This is the same as the removeCharacter method, except now you are dealing with a needle that is (possibly) longer than 1 character. You need to combine your solution to removeCharacter with your solution to findEndOfNeedle.
+
+### whatILike(String str)
+
+Consider the following examples:
+
+```
+I like tacos
+I like cake
+I like computers
+I like homework
+I like dancing
+```
+
+Is there a pattern to where the-thing-I-like is found in these strings? If you knew where the-thing-I-like started in the string, could you isolate it from the string?
+
+### getFirstNCharacters(String str, int n)
+
+What string method can you use to get a smaller string from a bigger string? Where does the smaller string start? Where does the smaller string end?
+
+### removeFirstNCharacters(String str, int n)
+
+This is the inverse of getFirstNCharacters. Where does the the small string you want start and end for this problem?
+
+### getLastNCharacters(String str, int n)
+
+This is similar to the getFirstNCharacters method, except now you want the last N characters. Where does this small string start? Where does it end? What string method can you use to identify how many characters are in the string? How can you calculate where this small string starts if you know the length of the string and that you want the last N characters?
+
+### removeLastNCharacters(String str, int n)
+
+This is the inverse of getLastNCharacters. Similar to how removeFirstNCharacters was the inverse of getFirstNCharacters.
+
+### frontBack(String str)
+
+You need to remove the first character then remove the last character, then concatenate them back together.
